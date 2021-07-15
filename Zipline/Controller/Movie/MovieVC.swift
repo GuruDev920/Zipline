ede147c901f5f5eec1eb7c209a2eeeccb0b4da1e
Sun <43668159+sunpooh@users.noreply.github.com>
2021-07-15 19:05:44 +0800

    movie screen tableview setup

--- Zipline/Controller/Movie/MovieVC.swift
+++ Zipline/Controller/Movie/MovieVC.swift
@@ -18,5 +18,46 @@ class MovieVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        read_movies()
+        setupTableView()
+    }
+    
+    func read_movies() {
+        
+    }
+    
+    func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = 120
+        tableView.register(UINib(nibName: "MovieCell", bundle: nil), forCellReuseIdentifier: "MovieCell")
+        tableView.mask = nil
+        tableView.reloadData()
+    }
+}
+
+extension MovieVC: UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+        
+        cell.movie = movies[indexPath.row]
+        
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        let contentHiehgt = scrollView.contentSize.height
+        
+        if (offsetY > contentHiehgt - scrollView.frame.height) && !isLoadingMore {
+            isLoadingMore = true
+            self.read_movies()
+        }
     }
 }
